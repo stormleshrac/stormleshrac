05376ea942d1e5847fc991df11819aeb5d4d0805
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2022-04-12 00:15:01 -0400

    ssssss
    sssssssssssssss

---
+++ config.py
@@ -0,0 +1,16 @@
+BOT_TOKEN = ''
+API_ID = 
+API_HASH = ''
+
+
+# DATOS DEL MOODLE
+USUARIO = ''
+PASSWORD = ''
+USUARIO_ID = 
+ZIP_MB = 
+MOODLE_URL = ''
+OWNER = []
+
+# CUENTA DE MEGA
+GMAIL_MEGA = 'yanco148@gmail.com'
+PASS_MEGA = 'yancarlos1482005'
