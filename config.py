c3308e9f1f91f98c10db9d92a9843b293a506589
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2022-04-13 11:48:02 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 BOT_TOKEN = '1722354800:AAGUhcgUbq1hUZKdMhkcioaPm_Lf91xrpto' #token de tu bot
 API_ID =  7739789 # lo creas en https://my.telegram.org/
 API_HASH = '0dac59387c482f674410f79636a09f1f'  # lo creas en https://my.telegram.org/
-OWNER = [XenoTks]
+OWNER = [1291931851]
 
 # DATOS DEL MOODLE
 USUARIO = 'daimecs'
