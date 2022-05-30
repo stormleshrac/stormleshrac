6f16a29703ab1ff61560cfcdae9fadc0949309d3
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2022-05-30 22:11:26 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+BOT_TOKEN = '5292156078:AAE0pR_-8rrlqodzDP-nB02vtg-ZdAGD_WU' #token de tu bot
+API_ID =  7739789 # lo creas en https://my.telegram.org/
+API_HASH = '0dac59387c482f674410f79636a09f1f'  # lo creas en https://my.telegram.org/
+OWNER = [1291931851]
+
+# DATOS DEL MOODLE
+USUARIO = 'frank.arean'
+MOODLE_PASSWORD = 'Zxcv,123'
+USUARIO_ID =  5127 #Tu id de la nube
+ZIP_MB = 100
+MOODLE_URL = 'eva.uo.edu.cu'
+
+# CUENTA DE MEGA - este bot sirve par mega tambien
+GMAIL_MEGA = '@gmail.com' 
+PASS_MEGA = 'contraseña'
