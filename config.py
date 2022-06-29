ffa42b76bb72d93e33bcd6e3531a8df2d394b63d
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2022-06-29 06:29:04 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-BOT_TOKEN = '5374616277:AAEi1BB39q8EB-2hYpoq_0l95quURlAlZcg' #token de tu bot
+BOT_TOKEN = '5549932725:AAHWpA4BpV-8cZARwdrwTGu4cShX_hNvGoQ' #token de tu bot
 API_ID =  7739789 # lo creas en https://my.telegram.org/
 API_HASH = '0dac59387c482f674410f79636a09f1f'  # lo creas en https://my.telegram.org/
 OWNER = [1291931851]
 
 # DATOS DEL MOODLE
-USUARIO = 'cbenavides'
-MOODLE_PASSWORD = 'Arleny2602*'
-USUARIO_ID =  9113 #Tu id de la nube
+USUARIO = 'mdramirez'
+MOODLE_PASSWORD = 'Gala2022*'
+USUARIO_ID =  2815 #Tu id de la nube
 ZIP_MB = 400
 MOODLE_URL = 'moodle.uclv.edu.cu'
 
