8f781c12ab1ff3ce8b760f5620ed71883c46d530
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2022-07-04 22:44:52 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-BOT_TOKEN = '5074502788:AAFqaW9vPzhxOHGiEaKoUqlGdvuLTZ4kbQ4' #token de tu bot
+BOT_TOKEN = '5497068174:AAEdGmwDKbv0GBCODTZesIXfSK4DP2J6jPY' #token de tu bot
 API_ID =  7739789 # lo creas en https://my.telegram.org/
 API_HASH = '0dac59387c482f674410f79636a09f1f'  # lo creas en https://my.telegram.org/
 OWNER = [1291931851]
 
 # DATOS DEL MOODLE
-USUARIO = 'frank.arean'
-MOODLE_PASSWORD = 'Zxcv,123'
-USUARIO_ID =  5127 #Tu id de la nube
-ZIP_MB = 99
-MOODLE_URL = 'eva.uo.edu.cu'
+USUARIO = 'avdul'
+MOODLE_PASSWORD = 'Micasaesgrande*123'
+USUARIO_ID =  7314 #Tu id de la nube
+ZIP_MB = 250
+MOODLE_URL = 'aulacened.uci.cu'
 
 # CUENTA DE MEGA - este bot sirve par mega tambien
 GMAIL_MEGA = '@gmail.com' 
