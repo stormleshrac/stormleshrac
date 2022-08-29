c71cecfe100a174107ca6ceed19650828b58e34c
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2022-08-28 17:45:37 -1200

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-BOT_TOKEN = '5549932725:AAHWpA4BpV-8cZARwdrwTGu4cShX_hNvGoQ' #token de tu bot
+BOT_TOKEN = '5383545798:AAGPgvjGT1po9jb-wAxuaf4eU24CJgEKnJ0' #token de tu bot
 API_ID =  7739789 # lo creas en https://my.telegram.org/
 API_HASH = '0dac59387c482f674410f79636a09f1f'  # lo creas en https://my.telegram.org/
 OWNER = [1291931851]
 
 # DATOS DEL MOODLE
-USUARIO = ''
-MOODLE_PASSWORD = ''
-USUARIO_ID =  #Tu id de la nube
+USUARIO = 'addgonzalez '
+MOODLE_PASSWORD = 'PlayMaker403*'
+USUARIO_ID = 11553 #Tu id de la nube
 ZIP_MB = 399
 MOODLE_URL = 'moodle.uclv.edu.cu'
 
