6023ecee37049d034c1d03f6f9dfcddf37b08bdd
stormleshrac <103478524+stormleshrac@users.noreply.github.com>
2022-09-07 15:05:20 -1200

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ API_HASH = '0dac59387c482f674410f79636a09f1f'  # lo creas en https://my.telegram
 OWNER = [1291931851]
 
 # DATOS DEL MOODLE
-USUARIO = 'pcarrazana'
-MOODLE_PASSWORD = 'sims.9812'
-USUARIO_ID = 2830 #Tu id de la nube
+USUARIO = ''
+MOODLE_PASSWORD = ''
+USUARIO_ID =  #Tu id de la nube
 ZIP_MB = 399
-MOODLE_URL = 'moodle.uclv.edu.cu'
+MOODLE_URL = ''
 
 # CUENTA DE MEGA - este bot sirve par mega tambien
 GMAIL_MEGA = '@gmail.com' 
